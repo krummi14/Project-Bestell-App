de3ae586b0f24d82219582668c81656eb68ba6d4
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-01-31 12:13:45 +0100

    feat: add templates of pizza and salad

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function init() {
+    renderDishes();
+}
+
+function renderDishes() {
     renderBurgers();
+    renderPizza();
+    renderSalad();
 }
 
 function renderBurgers() {
@@ -8,5 +14,23 @@ function renderBurgers() {
 
     for (let burgerIndex = 0; burgerIndex < 4; burgerIndex++) {
         contentBurger.innerHTML += getBurgerDishTemplate(burgerIndex);
+    }
+}
+
+function renderPizza() {
+    let contentBurger = document.getElementById('pizza_content');
+    contentBurger.innerHTML = "";
+
+    for (let pizzaIndex = 4; pizzaIndex < 8; pizzaIndex++) {
+        contentBurger.innerHTML += getPizzaDishTemplate(pizzaIndex);
+    }
+}
+
+function renderSalad() {
+    let contentBurger = document.getElementById('salad_content');
+    contentBurger.innerHTML = "";
+
+    for (let saladIndex = 8; saladIndex < myDishes.length; saladIndex++) {
+        contentBurger.innerHTML += getSaladDishTemplate(saladIndex);
     }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,13 +1,39 @@
 function getBurgerDishTemplate(burgerIndex) {
     return `<article class="dish">
                 <figure class="dish_img_and_description">
-                    <img class="burger_dish_img" src="${myDishes[burgerIndex].img}">
+                    <img class="dish_img" src="${myDishes[burgerIndex].img}">
                     <figcaption>
                         <h3>${myDishes[burgerIndex].name}</h3>
                         <p>${myDishes[burgerIndex].description}</p>
                     </figcaption>
                 </figure>
                 <p class="dish_price">${myDishes[burgerIndex].price}€</p>
+            </article>`;
+}
+
+function getPizzaDishTemplate(pizzaIndex) {
+    return `<article class="dish">
+                <figure class="dish_img_and_description">
+                    <img class="dish_img dish_img_position_${pizzaIndex}" src="${myDishes[pizzaIndex].img}">
+                    <figcaption>
+                        <h3>${myDishes[pizzaIndex].name}</h3>
+                        <p>${myDishes[pizzaIndex].description}</p>
+                    </figcaption>
+                </figure>
+                <p class="dish_price">${myDishes[pizzaIndex].price}€</p>
+            </article>`;
+}
+
+function getSaladDishTemplate(saladIndex) {
+    return `<article class="dish">
+                <figure class="dish_img_and_description">
+                    <img class="dish_img" src="${myDishes[saladIndex].img}">
+                    <figcaption>
+                        <h3>${myDishes[saladIndex].name}</h3>
+                        <p>${myDishes[saladIndex].description}</p>
+                    </figcaption>
+                </figure>
+                <p class="dish_price">${myDishes[saladIndex].price}€</p>
             </article>`;
 }
 
