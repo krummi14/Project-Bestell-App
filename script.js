5c6ee1be6cc14ad352cac89ddc120679323b8654
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-02-03 11:26:06 +0100

    style: clean code

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let contentBurger = document.getElementById('burger_content');
+let contentPizza = document.getElementById('pizza_content');
+let contentSalad = document.getElementById('salad_content');
+let contentBasket = document.getElementById('basket_content');
+let contentDishPrice = document.getElementById('dish_price');
+let contentTotalPrice = document.getElementById('total_price');
+let contentTableTotalPrice = document.getElementById('total_table_price');
+
 function init() {
     renderDishes();
     addBasket();
@@ -10,7 +18,6 @@ function renderDishes() {
 }
 
 function renderBurgers() {
-    let contentBurger = document.getElementById('burger_content');
     contentBurger.innerHTML = "";
 
     for (let burgerIndex = 0; burgerIndex < 4; burgerIndex++) {
@@ -19,25 +26,22 @@ function renderBurgers() {
 }
 
 function renderPizza() {
-    let contentBurger = document.getElementById('pizza_content');
-    contentBurger.innerHTML = "";
+    contentPizza.innerHTML = "";
 
     for (let pizzaIndex = 4; pizzaIndex < 8; pizzaIndex++) {
-        contentBurger.innerHTML += getPizzaDishTemplate(pizzaIndex);
+        contentPizza.innerHTML += getPizzaDishTemplate(pizzaIndex);
     }
 }
 
 function renderSalad() {
-    let contentBurger = document.getElementById('salad_content');
-    contentBurger.innerHTML = "";
+    contentSalad.innerHTML = "";
 
     for (let saladIndex = 8; saladIndex < myDishes.length; saladIndex++) {
-        contentBurger.innerHTML += getSaladDishTemplate(saladIndex);
+        contentSalad.innerHTML += getSaladDishTemplate(saladIndex);
     }
 }
 
 function addBasket() {
-    let contentBasket = document.getElementById('basket_content');
     contentBasket.innerHTML = getBasketTemplate();
 }
 
@@ -50,29 +54,6 @@ function addDish(orderIndex) {
 function formatPrice(price) {
     return price.toFixed(2).replace(".", ",") + "€";
 }
-
-//function calculateDishPrice(orderIndex) {
-//let contentDishPrice = document.getElementById('dish_price');
-//let contentTotalPrice = document.getElementById('total_price');
-//let contentTableTotalPrice = document.getElementById('total_table_price');
-//let currentSubTotal = contentDishPrice;
-//currentSubTotal = parseFloat(currentSubTotal.replace(",", "."));
-
-//let deliveryFee = 4.99;
-//let newDishPrice = 0;
-
-//let dishPrice = myDishes[orderIndex].price;
-//dishPrice = parseFloat(dishPrice.replace(",", "."));
-
-//newDishPrice = currentSubTotal + dishPrice;
-//newDishPrice = dishPrice++;
-//let subtotalPrice = dishPrice + deliveryFee;
-//let totalPrice = subtotalPrice;
-
-//contentDishPrice.innerHTML = formatPrice(newDishPrice);
-//contentTotalPrice.innerHTML = "(" + formatPrice(totalPrice) + ")";
-//contentTableTotalPrice.innerHTML = formatPrice(totalPrice);
-//}
 
 function calculateDishPrice(orderIndex) {
     let contentDishPrice = document.getElementById('dish_price');
