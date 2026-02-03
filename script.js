f1ef72bbddb86d03303b4fbddcef9ac175ca8f7f
Daniel Krummhauer <d.krummhauer@gmail.com>
2026-02-03 11:15:43 +0100

    feat: add calculat-methode in basket

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function init() {
     renderDishes();
+    addBasket();
 }
 
 function renderDishes() {
@@ -35,8 +36,65 @@ function renderSalad() {
     }
 }
 
+function addBasket() {
+    let contentBasket = document.getElementById('basket_content');
+    contentBasket.innerHTML = getBasketTemplate();
+}
+
 function addDish(orderIndex) {
     let contentOrder = document.getElementById('order_content');
     contentOrder.innerHTML += getOrderDishTemplate(orderIndex);
+    calculateDishPrice(orderIndex);
 }
+
+function formatPrice(price) {
+    return price.toFixed(2).replace(".", ",") + "€";
+}
+
+//function calculateDishPrice(orderIndex) {
+//let contentDishPrice = document.getElementById('dish_price');
+//let contentTotalPrice = document.getElementById('total_price');
+//let contentTableTotalPrice = document.getElementById('total_table_price');
+//let currentSubTotal = contentDishPrice;
+//currentSubTotal = parseFloat(currentSubTotal.replace(",", "."));
+
+//let deliveryFee = 4.99;
+//let newDishPrice = 0;
+
+//let dishPrice = myDishes[orderIndex].price;
+//dishPrice = parseFloat(dishPrice.replace(",", "."));
+
+//newDishPrice = currentSubTotal + dishPrice;
+//newDishPrice = dishPrice++;
+//let subtotalPrice = dishPrice + deliveryFee;
+//let totalPrice = subtotalPrice;
+
+//contentDishPrice.innerHTML = formatPrice(newDishPrice);
+//contentTotalPrice.innerHTML = "(" + formatPrice(totalPrice) + ")";
+//contentTableTotalPrice.innerHTML = formatPrice(totalPrice);
+//}
+
+function calculateDishPrice(orderIndex) {
+    let contentDishPrice = document.getElementById('dish_price');
+    let contentTotalPrice = document.getElementById('total_price');
+    let contentTableTotalPrice = document.getElementById('total_table_price');
+
+    let currentSubTotal = contentDishPrice.innerHTML;
+    let currentTotalPrice = contentTableTotalPrice.innerHTML;
+    let newDishPrice = 0;
+    let newTotalPrice = 0;
+    let dishPrice = myDishes[orderIndex].price;
+
+    currentSubTotal = parseFloat(currentSubTotal.replace(",", "."));
+    currentTotalPrice = parseFloat(currentTotalPrice.replace(",", "."));
+    dishPrice = parseFloat(dishPrice.replace(",", "."));
+
+    newDishPrice = currentSubTotal + dishPrice;
+    newTotalPrice = currentTotalPrice + dishPrice;
+
+    contentDishPrice.innerHTML = formatPrice(newDishPrice);
+    contentTotalPrice.innerHTML = "(" + formatPrice(newTotalPrice) + ")";
+    contentTableTotalPrice.innerHTML = formatPrice(newTotalPrice);
+}
+
 
--- scripts/template.js
+++ scripts/template.js
@@ -59,4 +59,29 @@ function getOrderDishTemplate(orderIndex) {
                 </span>
             </div>`
 }
+
+function getBasketTemplate() {
+    return `<h3>Your Basket</h3>
+            <span id="order_content" class="order_content_gap_and_pad">
+
+            </span>
+            <table>
+                <tr>
+                    <td class="text_left">Subtotal</td>
+                    <td class="text_right"><div id="dish_price">0,00€</div></td>
+                </tr>
+                <tr class="table_padding_bottom">
+                    <td class="text_left">Delivery Fee</td>
+                    <td class="text_right">4,99€</td>
+                </tr>
+                <tr class="table_border_top">
+                    <th class="text_left">Total</th>
+                    <th class="text_right"><div id="total_table_price">4,99€</div></th>
+                </tr>
+            </table>
+            <span class="order_button_content">
+                <button class="order_button">Buy now<div id="total_price">(0,00€)</div></button>
+            </span>`
+}
+
 console.log(myDishes);
